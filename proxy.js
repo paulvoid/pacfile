9c09feeb103a1c13853579e374da783a1490ea0c
paulvoid <40271180+paulvoid@users.noreply.github.com>
2022-11-23 17:35:31 +0700

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -4,6 +4,10 @@ function FindProxyForURL(url, host) {
     if (dnsDomainIs(host, "ego-d.xyz")){
         return "PROXY 127.0.0.1:8000";
     }
+    // roblox.com
+    if (dnsDomainIs(host, "roblox.com")) || (shExpMatch(url, "*.roblox.com")) {
+        return "PROXY 127.0.0.1:8000";
+    }
 
     // by default use no proxy
     return "DIRECT";
